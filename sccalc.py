f1ebe0bc629feac9d30ed636f1adef22c52ee93f
CoreTurboBoost <github.com/CoreTurboBoost>
2025-07-23 09:24:25 +0100

    Fix errors in new enforce_command_required_paramters function. Add missing information from output

--- sccalc.py
+++ sccalc.py
@@ -614,23 +614,23 @@ if len(sys.argv) > 1:
             elif operator == "<":
                 return left < right
             return None
-        def enforce_command_required_parameters(command_name: str, required_arguments: list[str], required_args_repeat: bool, optional_arguments: list[str], optional_args_repeat: bool, expression_split_count: int) -> None:
+        def enforce_command_required_parameters(command_name: str, required_arguments: list[str], required_args_repeat: bool, optional_arguments: list[str], optional_args_repeat: bool, expression_split_count: int, line_index: int) -> None:
             if expression_split_count < len(required_arguments):
                 required_arguments_str = ""
                 optional_arguments_str = ""
                 for i, required_arg in enumerate(required_arguments):
                     if i != 0:
                         required_arguments_str += " "
-                    required_arguments_str += f"<required_arg>"
+                    required_arguments_str += f"<{required_arg}>"
                 if required_args_repeat:
                     required_arguments_str += "..."
                 for i, optional_arg in enumerate(optional_arguments):
                     if i != 0:
                         optional_arguments_str += " "
-                    optional_arguments_str += f"[optional_arg]"
+                    optional_arguments_str += f"[{optional_arg}]"
                 if optional_args_repeat:
                     optional_arguments_str += "..."
-                sys.exit(f"Error: {command_name}: Missing required arguments, expected '!{command_name} {required_arguments_str} {optional_arguments_str}'")
+                sys.exit(f"[{line_index+1}] Error: {command_name}: Missing required arguments, expected '!{command_name} {required_arguments_str} {optional_arguments_str}'")
         fh = open(sys.argv[1])
         contents = fh.read().split("\n")
         contents = list(zip(contents, range(0, len(contents))))
