3538f55b6f6c68621c7f1f4f3de3c34912814bbf
CoreTurboBoost <github.com/CoreTurboBoost>
2025-08-23 06:38:00 +0100

    Add a 2-Clause BSD license to the program

--- sccalc.py
+++ sccalc.py
@@ -11,13 +11,36 @@ import string
 import itertools
 
 APP_VERSION_MAJOR = 3
-APP_VERSION_MINOR = 6
+APP_VERSION_MINOR = 7
 APP_SCRIPT_VERSION = 6
 
 CUSTOM_SCRIPT_VERSION = False
 
 variables = {"script_version": APP_SCRIPT_VERSION}
 iterator_arrays = {} # {str: list[decimal.Decimal]}
+
+PROGRAM_LICENSE = """
+Copyright 2025 CoreTurboBoost
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+def print_program_license():
+    print(PROGRAM_LICENSE)
 
 def log10(x):
     return math.log(x, 10)
@@ -1448,6 +1471,9 @@ if len(sys.argv) > 1:
         sys.exit()
     if (sys.argv[1] == "__VERSION__"):
         print(f"{APP_VERSION_MAJOR}.{APP_VERSION_MINOR}")
+        sys.exit()
+    if sys.argv[1] == "--license":
+        print_program_license()
         sys.exit()
     if sys.argv[1] == "--output-script-standard":
         standards_output_path = f"script-{APP_SCRIPT_VERSION}-standard"
